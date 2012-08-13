84e0121270b21e4745ec65ac617fc1c77f37e45c
Colin Mitchell <muffinista@gmail.com>
2012-08-13 11:43:28 -0400

    fixes

--- app/helpers/mail_dump/application_helper.rb
+++ app/helpers/mail_dump/application_helper.rb
@@ -1,4 +1,10 @@
 module MailDump
   module ApplicationHelper
+    def output_html(m)
+      m.html.respond_to?(:body) ? m.html.body.to_s : m.html
+    end
+    def output_text(m)
+      m.body.respond_to?(:body) ? m.body.body.to_s : m.body
+    end
   end
 end
